a8eb520140d345fbfa9c240596a5a0330b7f0c29
Otter963 <averagetoast963@gmail.com>
2025-10-13 12:24:54 +0200

    SampleScene | Did some stuff

--- CatsVSDogs_clone_0/Assets/Scripts/SimpleScore.cs
+++ CatsVSDogs_clone_0/Assets/Scripts/SimpleScore.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using Unity.Netcode;
+using TMPro;
 
 public class SimpleScore : NetworkBehaviour
 {
     public NetworkVariable<int> score = new NetworkVariable<int>();
+
+    public NetworkVariable<string> scoreStringText = new NetworkVariable<string>();
+
+    public TextMeshProUGUI scoreText;
 
     void Start()
     {
         score.OnValueChanged += (oldValue, newValue) =>
         {
             Debug.Log($"Score changed to: {newValue}");
+            scoreStringText.Value = score.Value.ToString();
         };
     }
 
@@ -18,6 +24,7 @@ public class SimpleScore : NetworkBehaviour
         if (IsServer && Input.GetKeyDown(KeyCode.S))
         {
             score.Value += 10;
+            scoreText.text = scoreStringText.ToString();
             Debug.Log("Server added 10 points");
         }
     }
